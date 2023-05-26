18467caa2a41bb7eb7c563ad4d93cfff93e2419b
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-05-26 12:20:43 -0400

    Created a string to be used as a parameter

---
+++ script.js
@@ -0,0 +1,18 @@
+const str = "The most beautiful girl on earth"
+
+//console.log(str);
+let newArray = str.split(" ");
+//console.log(newArray);
+//console.log(newArray[1].length);
+
+findLongestWord = (str) => {
+	let newArray = str.split(" ");
+	let longWord = 0;
+	for (var i = 0; i < newArray.length; i++) {
+		if (newArray[i].length > longWord) {
+			longWord = newArray[i].length;
+		}
+	}
+	return longWord;
+}
+console.log(findLongestWord(str));
