08375f3a58ec3118135f7592e97bd92db8bab57e
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-05-28 10:13:19 -0400

    Modified the logic to match changes in code

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ findLongestWord = (str) => {
 }
 /* 
 1. Used arrow function form to set "str" as the parameter
-2. Defined the necessary variable inside the function; created "logWord" and set it to the value if a string element
-3. logWord will store longest element as the function loops through the array
-4. Beginning with index 0, the function will move along and find the length of each element using the .length property
-5. The longest element in the array will be stored as the "longWord" value until all elements are evaluated
-6. When called, the function will return the value of "logWord", which is the value of the longest element
+2. Defined the necessary variable inside the function; created "logWord" and set it to the value of an element in th new array
+3. longWord will store an element in the array
+4. the function will loop through each element in the array evaluating the length
+5. if the legnth of the next element is longer than the previous element, that element will now become the value of longWord
+6. the function will return the the actual word stored in longWord when called
 */
 
 console.log(findLongestWord(str));
